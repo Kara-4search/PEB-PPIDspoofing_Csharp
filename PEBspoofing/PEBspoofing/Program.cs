81c1974d8135948cf57917713ed52d4560f4d6b7
Kara-4search <rubyistctrldyt@gmail.com>
2021-08-05 17:58:28 +0800

    README.md & Code update

--- PEBspoofing/PEBspoofing/Program.cs
+++ PEBspoofing/PEBspoofing/Program.cs
@@ -4,16 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace PEBspoofing
 {
 
     class Program
     {
+        public static int FindProcessIDByName(string processname)
+        {
+            int processpid = 0;
+
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process p in processlist)
+            {
+                // Console.WriteLine("Process: {0} ID: {1}", p.ProcessName, p.Id);
+                if (p.ProcessName.ToLower() == processname)
+                {
+                    // Console.WriteLine("Find: {0}", p.Id);
+                    // System.Threading.Thread.Sleep(50000);
+                    processpid = p.Id;
+                    return processpid;
+                }
+                // System.Threading.Thread.Sleep(100);
+            }
+            return processpid;
+        }
+
         static void Main(string[] args)
         {
-            int processpid = 6344;
-            ProcessCreator.CreateProcess(processpid);
+            int processpid = FindProcessIDByName("explorer");
+            // Console.WriteLine(processpid);
+            // System.Threading.Thread.Sleep(50000);
+            if (processpid != 0)
+            {
+                ProcessCreator.CreateProcess(processpid);
+            }
+            
             //IntPtr test = IntPtr.Zero;
             //Char[] arrayC = new char[64];
             //ProcessCreator.CURDIR test = new ProcessCreator.CURDIR();
